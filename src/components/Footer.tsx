ef6ac00bd9563543f6dd553f1db92f7c644fae09
Robson Lopes Cavalcante <roblucci100@gmail.com>
2025-10-04 09:08:22 -0300

    update: footer modificado

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="text-center py-4 text-sm">
-      Itinerar.io - Unleashed Giants 2025
+      <div className="mx-auto max-w-7xl">
+        <p className="text-gray-500">
+          © {currentYear} Itinerar - Todos os direitos reservados.
+        </p>
+        <div className="text-xs text-gray-400 mt-2">Unleashed Giants - Awin</div>
+      </div>
     </footer>
   )
 }
