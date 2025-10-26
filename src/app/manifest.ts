f33e02c119609367990879c3246a82e858b7a7d5
Robson Lopes Cavalcante <roblucci100@gmail.com>
2025-10-26 11:19:11 -0300

    fix: correção no nome

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Itinerar.com.br",
+    name: "Itinerar",
     short_name: "Itinerar",
     description: "Descubra os melhores pontos turísticos e o que fazer em qualquer destino, como São Paulo, Paris, Londres e muito mais. Crie roteiros de viagem personalizados com as melhores dicas e atrações.",
     display: "standalone",
